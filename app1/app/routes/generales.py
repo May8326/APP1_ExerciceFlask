39e39b23b3bda41a3a6757d0aa0d0d0944e2dc35
May8326 <maelys.gioan@outlook.fr>
2024-12-18 09:44:55 +0100

    fait exo

--- app1/app/routes/generales.py
+++ app1/app/routes/generales.py
@@ -1,6 +1,10 @@
 from ..app import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route("/pays/<string:nom>")
 def pays(nom):
     return render_template("pages/pays.html", pays=nom)
+
+@app.route("/retrieve_wikidata/<string:id>")
+def retrieve_wikidata(id):
+    return render_template("pages/retrieve_wikidata.html", id=id)
