336f43b96ba473dc1156f001a2c3b87ffbf04e97
May8326 <maelys.gioan@outlook.fr>
2024-12-19 12:15:23 +0100

    config route wikidata

--- app1/app/routes/generales.py
+++ app1/app/routes/generales.py
@@ -1,10 +1,20 @@
 from ..app import app
-from flask import render_template, request
+from flask import render_template
+from ..config import WIKIDATA_API_URL
+import requests
 
+"""
 @app.route("/pays/<string:nom>")
 def pays(nom):
     return render_template("pages/pays.html", pays=nom)
+"""
 
-@app.route("/retrieve_wikidata/<string:id>")
+# définition de la route interroger l'API de wikidata
+@app.route("/retrieve_wikidata/<id>", methods=["GET"])
 def retrieve_wikidata(id):
-    return render_template("pages/retrieve_wikidata.html", id=id)
+    params = {
+        "action": "wbgetentities",
+        "ids": id,
+        "format": "json"
+    }
+    return render_template("pages/retrieve_wikidata.html")
