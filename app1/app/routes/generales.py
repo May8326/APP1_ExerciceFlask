0be5672c5869c0e096f39ad00e5150465adbd6ed
May8326 <maelys.gioan@outlook.fr>
2024-12-20 09:37:46 +0100

    correction de la route

--- app1/app/routes/generales.py
+++ app1/app/routes/generales.py
@@ -17,33 +17,16 @@ def retrieve_wikidata(id):
     params = {
         "action": "wbgetentities",
         "ids": id,
-        "format": "json"
+        "format": "json",
+        "languages": "fr"
     }
 
+    # Appel de l'API
+
     try:
-        # requête GET vers l'API de wikidata
-        reponse = requests.get(WIKIDATA_API_URL, params=params)
-        reponse.raise_for_status()
-
-        # Décodage des données JSON
-        donnees = reponse.json()
-        id_donnees = donnees.get('entites', {}).get(id, {})
-
-        # Verifier si l'entité existe
-        if id_donnees:
-            return render_template("pages/retrieve_wikidata.html", 
-                                   status_code=reponse.status_code,
-                                   id_donnees=id_donnees, 
-                                   wikidata_id=id,
-                                   content_type=reponse.headers.get('content-type'))
-        # message d'erreur si l'entité n'existe pas
-        else:
-            return render_template("pages/retrieve_wikidata.html", 
-                                   status_code=reponse.status_code,
-                                   content_type=reponse.headers.get('content-type'),
-                                   error_message=f"Aucune donnée valide n'a été retournée pour l'ID \"{id}\".")
+        return requests.get(WIKIDATA_API_URL, params=params)
     
-    except requests.exceptions.RequestException as e:
-            return render_template("pages/retrieve_wikidata.html", 
-                                    status_code=500,
-                                    error_message=str(e))
+# Message d'erreur si l'ID n'est pas valide
+
+    except:
+        return f"Aucune donnée valide n'a été retournée pour l'ID {id}"
