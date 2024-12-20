bda1e24e3cccb19cb2004a179d8c63b6d52a86e7
May8326 <maelys.gioan@outlook.fr>
2024-12-20 09:52:54 +0100

    requête du data de l'API fonctionne, lier avec le Jinja du HTML

--- app1/app/routes/generales.py
+++ app1/app/routes/generales.py
@@ -22,9 +22,14 @@ def retrieve_wikidata(id):
     }
 
     # Appel de l'API
+    data = requests.get(WIKIDATA_API_URL, params=params)
+    # Récupération du JSON
+    data = data.json()
 
+    # Requête
     try:
-        return requests.get(WIKIDATA_API_URL, params=params)
+        #return data (test)
+        return render_template("pages/retrieve_wikidata.html")  # Renvoie les données de l'entité
     
 # Message d'erreur si l'ID n'est pas valide
 
