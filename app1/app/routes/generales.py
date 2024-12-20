e1e2dca503275830ffdd8a743afd1017e935f2ca
May8326 <maelys.gioan@outlook.fr>
2024-12-20 18:31:49 +0100

    corrigé les messages d'erreur qui n'apparaissaient pas sur le HTML

--- app1/app/routes/generales.py
+++ app1/app/routes/generales.py
@@ -3,11 +3,6 @@ from flask import request, render_template
 from ..config import WIKIDATA_API_URL
 import requests
 
-"""
-@app.route("/pays/<string:nom>")
-def pays(nom):
-    return render_template("pages/pays.html", pays=nom)
-"""
 
 # définition de la route interroger l'API de wikidata
 @app.route("/retrieve_wikidata/<id>", methods=["GET"])
@@ -27,16 +22,26 @@ def retrieve_wikidata(id):
     # définition des valeurs pour le jinja
 
 
-    # Requête
-    try:
-        #return data ()
+    # Vérification des erreurs
+    if "error" in data_json:
+        # Récupère l'erreur
+        error_message = data_json["error"]["info"]
+        # Renvoie le HTML avec l'erreur
+        return render_template(
+            "pages/retrieve_wikidata.html",
+            wikidata_id=id,
+            status_code=data.status_code,
+            content_type=data.headers.get("Content-Type"),
+            error_message=error_message  # Transmettre l'erreur pour le HTML
+        )
+    
+    # Si tout a marché
+    else:
+        # Renvoie le HTML avec le JSON
         return render_template("pages/retrieve_wikidata.html", 
                                wikidata_id = id,
                                status_code = data.status_code,
                                content_type = data.headers.get('Content-Type'), 
-                               data_json = data_json)  # Renvoie les données de l'entité
+                               wikidata_json = data_json,
+                               error_message = None) 
     
-# Message d'erreur si l'ID n'est pas valide
-    
-    except:
-        return f"Aucune donnée valide n'a été retournée pour l'ID {id}"
